29ec4499dc6883e8d21ae2e5951d1279f30022e5
Kirsten Demko <105938438+kirstendanica@users.noreply.github.com>
2023-04-24 13:51:21 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,31 +1,46 @@
-// Get all sections that have an ID defined
+
+//////////////////////////////// FIXED SIDEBAR NAVITGATION WITH SCROLL //////////////////////////////////
+
+// All of the navigational sections have IDs
 const sections = document.querySelectorAll("section[id]");
 
-// Add an event listener listening for scroll
-window.addEventListener("scroll", navHighlighter);
+// Add the event listener for scrolling
+window.addEventListener("scroll", navItemDisplay);
 
-function navHighlighter() {
+function navItemDisplay() {
   
-  // Get current scroll position
+  // This is the current scroll position
   let scrollY = window.pageYOffset;
   
-  // Now we loop through sections to get height, top and ID values for each
+  // Use a loop to go through sections to get their values
   sections.forEach(current => {
     const sectionHeight = current.offsetHeight;
     const sectionTop = current.offsetTop - 50;
-    sectionId = current.getAttribute("id");
+    // The navSectionId variable shows which link needs an active class
+    navSectionId = current.getAttribute("id"); 
     
     /*
-    - If our current scroll position enters the space where current section on screen is, add .active class to corresponding navigation link, else remove it
-    - To know which link needs an active class, we use sectionId variable we are getting while looping through sections as an selector
+    Use 'if else' statement to determine the current section of the page and use the 'active' class to initiate the feature or remove it as necessary to the position
+
     */
     if (
       scrollY > sectionTop &&
       scrollY <= sectionTop + sectionHeight
     ){
-      document.querySelector(".navigation a[href*=" + sectionId + "]").classList.add("active");
+      document.querySelector(".navigation a[href*=" + navSectionId + "]").classList.add("active");
     } else {
-      document.querySelector(".navigation a[href*=" + sectionId + "]").classList.remove("active");
+      document.querySelector(".navigation a[href*=" + navSectionId + "]").classList.remove("active");
     }
   });
 }
+
+//////////////////////////////// NAVIGATION TOGGLER FOR DIFFERENT VIEWPORTS //////////////////////////////////
+
+// Assign variables to the toggler button and navigation (use querySelector for classes)
+const togglerNav = document.querySelector(".toggler");
+const navMenu = document.querySelector(".toggler-open");
+
+// Add the event listeners for when the toggler menu is clicked and show it
+button.addEventListener("click", () => {
+  navMenu.classList.toggle(".show");
+});
